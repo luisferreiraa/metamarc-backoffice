423420e101a8bdb60c99eada90b6b1ac8218e767
luisferreiraa <luis.c.ferreira.prt_a@msft.cesae.pt>
2025-07-09 15:50:48 +0100

    09-07-2025_15:50

--- src/app/admin/tiers/page.tsx
+++ src/app/admin/tiers/page.tsx
@@ -2,6 +2,8 @@ import { TiersManagement } from "@/components/admin/tiers/tiers-management";
 import { AuthGuard } from "@/components/auth/auth-guard";
 import { getTiers } from "@/lib/actions/tier-actions";
 
+export const dynamic = "force-dynamic"
+
 export default async function TiersPage() {
 
     const initialTiers = await getTiers()
